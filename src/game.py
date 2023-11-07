ab40cce41e19cdaf5a82c3242078e7da53bdb176
ak2k2 <armaan.kapoor@cooper.edu>
2023-11-07 07:33:55 -0500

    AI vs AI

--- src/game.py
+++ src/game.py
@@ -57,8 +57,9 @@ def human_vs_AI(who_moves_first, human_color):
             print(f"It's {human_color.name}'s Turn.\n")
             print_legal_moves(legal_moves)
             selected_move_index = int(input("Choose your move by index: "))
+
             if selected_move_index not in range(len(legal_moves)):
-                print("Invalid index selected. Try again.")
+                print("Invalid index selected. Try again...")
                 continue
             selected_move = legal_moves[selected_move_index]
 
@@ -107,47 +108,66 @@ def human_vs_AI(who_moves_first, human_color):
     print(f"Game lasted {move_count} moves!")
 
 
-# def AI_vs_AI():
-#     WP, BP, K = initialize_game()
-#     current_player = PlayerTurn.BLACK
-#     move_count = 0
-#     max_depth = 7
+def AI_vs_AI(who_moves_first, max_depth=7, time_limit=5):
+    WP, BP, K = initialize_game()
+    current_player = who_moves_first
 
-#     print_board(WP, BP, K)
+    move_count = 0
+    game_over = False
 
-#     while move_count < 100:
-#         if current_player == PlayerTurn.BLACK:
-#             legal_moves = generate_legal_moves(WP, BP, K, current_player)
+    print_board(WP, BP, K)
+    while move_count < 100 and not game_over:
+        print(f"It's {current_player.name}'s Turn.\n")
+        start_time = time.time()
 
-#             if not legal_moves:
-#                 print(f"GAME OVER. {current_player.name} LOSES!")
-#                 break
-#             print(f"It's {current_player.name}'s Turn.\n")
-#             print_legal_moves(legal_moves)
-#             selected_move = legal_moves[int(input("Choose your move by index: "))]
+        legal_moves = generate_legal_moves(WP, BP, K, current_player)
+        if not legal_moves:
+            print(f"GAME OVER. {current_player.name} LOSES!")
+            game_over = True
+            break
 
-#             print(f"Move chosen: {convert_move_list_to_pdn([selected_move])}")
-#             WP, BP, K = do_move(WP, BP, K, selected_move, current_player)
+        immediate_move = len(legal_moves) == 1
+        if immediate_move:
+            print(
+                f"AI ({current_player.name}): Took immediate move due to only one legal move available."
+            )
+            best_move = legal_moves[0]
+        else:
+            best_move, depth_reached = AI(
+                (WP, BP, K), current_player, max_depth, time_limit
+            )
 
-#         else:  # AI's turn
-#             print("AI is thinking...")
-#             best_move = AI((WP, BP, K), max_depth)
-#             if best_move is None:
-#                 print(f"GAME OVER. {current_player.name} LOSES!")
-#                 break
-#             print(f"AI chose move: {convert_move_list_to_pdn([best_move])}")
-#             WP, BP, K = do_move(WP, BP, K, best_move, current_player)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
 
-#         print(f"HEURISTIC: {basic_heuristic(WP, BP, K)}")
-#         print_board(WP, BP, K)
-#         print("-" * 50 + "\n")
+        if best_move is None:
+            print(f"GAME OVER. {current_player.name} LOSES!")
+            game_over = True
+            break
 
-#         current_player = (
-#             PlayerTurn.WHITE if current_player == PlayerTurn.BLACK else PlayerTurn.BLACK
-#         )
-#         move_count += 1
+        move_description = convert_move_list_to_pdn([best_move])
+        print(f"AI ({current_player.name}) MOVED: {move_description}")
+        WP, BP, K = do_move(WP, BP, K, best_move, current_player)
 
-#     print(f"Game over in {move_count} moves.")
+        if not immediate_move:
+            if elapsed_time <= time_limit:
+                print(
+                    f"AI ({current_player.name}) searched to depth {depth_reached} in {elapsed_time:.2f} seconds."
+                )
+            else:
+                print(
+                    f"AI ({current_player.name}) hit the 'time limit' and reached depth {depth_reached}."
+                )
+
+        print(f"EVAL: {basic_heuristic(WP, BP, K)}")
+        print_board(WP, BP, K)
+        print("-" * 50 + "\n")
+
+        current_player = switch_player(current_player)
+        move_count += 1
+
+    if not game_over:
+        print(f"GAME OVER in {move_count} moves! DRAW.")
 
 
 # def random_vs_AI():
@@ -220,5 +240,5 @@ def simulate_random_games(n, first_player=PlayerTurn.WHITE):
 if __name__ == "__main__":
     # simulate_random_games(10000, first_player=PlayerTurn.WHITE)
     # human_vs_human()
-    human_vs_AI(who_moves_first=PlayerTurn.BLACK, human_color=PlayerTurn.BLACK)
+    human_vs_AI(who_moves_first=PlayerTurn.BLACK, human_color=PlayerTurn.WHITE)
     # random_vs_AI()
